5287e60947cde90283ae244fd171a48e8b852e38
Warnierr <rww.warnier@gmail.com>
2023-08-15 02:54:47 +0200

    update, add ssl autorization

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 import App from "./App";
 
+/*
 const client = new ApolloClient({
   uri: 'http://localhost:2021/graphql',
   cache: new InMemoryCache(),
 });
-
+*/
 ReactDOM.render(
   <React.StrictMode>
-      <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>,
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+/*
+      <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  */
