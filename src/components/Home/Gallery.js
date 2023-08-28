bbbe516b9e5e6d585e50ccc11b3bc59ffc9e3b1a
Warnierr <rww.warnier@gmail.com>
2023-08-28 19:52:36 +0200

    update gallery

--- src/components/Home/Gallery.js
+++ src/components/Home/Gallery.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_PRODUCTS } from '../../lib/queries';
+
 const styles = {
     gallery : {
       height: 'calc(100vh - 120px)', 
@@ -8,6 +11,7 @@ const styles = {
 
 // main
 function Gallery() {
+  const { loading, error, data } = useQuery(GET_PRODUCTS, { variables : {category : "women"}});
     return(<div className="col-md-8 order-md-2 col-lg-9">
       <div className="container-fluid" style={styles.gallery}>
         <div className="row">
