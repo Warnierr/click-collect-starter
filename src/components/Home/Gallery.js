16e47ebd1b29e55241e59502a21c37263b4750f3
Warnierr <rww.warnier@gmail.com>
2023-08-29 08:51:37 +0200

    add card

--- src/components/Home/Gallery.js
+++ src/components/Home/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_PRODUCTS } from '../../lib/queries';
+import Card from './Card'
 
 const styles = {
     gallery : {
@@ -11,11 +12,18 @@ const styles = {
 
 // main
 function Gallery() {
-  const { loading, error, data } = useQuery(GET_PRODUCTS, { variables : {category : "women"}});
+  const { loading, error, data } = useQuery(
+    GET_PRODUCTS, { variables : {category : "women"},
+  });
+  if (loading) return <div>loading ...</div>
+  if (error) return <div>sorry an error occured {error}</div>
+  console.dir(data);
     return(<div className="col-md-8 order-md-2 col-lg-9">
       <div className="container-fluid" style={styles.gallery}>
         <div className="row">
-            {/* products */}
+            {data?.products.map((product) => (
+              <Card {...product} />
+            ))}
         </div>
       </div>
     </div>)
